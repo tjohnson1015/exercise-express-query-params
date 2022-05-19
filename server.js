7964d2dc3d8d7b43028be1bc9a07d754a8168388
Lachlan Heywood <lachieh@users.noreply.github.com>
2022-05-19 17:35:21 -0400

    add initial solution

--- server.js
+++ server.js
@@ -12,8 +12,35 @@ const productsService = require('./services/products')
 app.get('/', (req, res) => res.send('Build the API!'))
 
 // Build Routes
+// Get All Products
+app.get('/api/v1/products', (req, res) => {
+  // use service to get all products
+  const products = productsService.findAll()
+  // send response
+  res.json(products)
+})
 
+// Get Product By Id
+app.get('/api/v1/products/:id', (req, res) => {
+  // check if id is a valid number
+  const id = Number(req.params.id)
+  // if not a valid number, send error response and return
+  if (isNaN(id)) {
+    res.status(400).json({ error: 'Invalid ID' })
+    return
+  }
 
+  // if valid, find product by id
+  const product = productsService.findOneById(id)
+  // if not found, send error response and return
+  if (!product) {
+    res.status(404).json({ error: 'Product not found' })
+    return
+  }
+
+  // if found, send product response
+  res.json(product)
+})
 
 app.get('*', (req, res) => res.status(404).send('Page not found'))
 
