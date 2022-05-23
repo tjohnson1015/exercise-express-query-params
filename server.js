31c689e8899a7d2af966a28bbaabcff32b6734e5
Tyler Johnson <tylerjohnson1015@yahoo.com>
2022-05-23 08:52:01 -0400

    Get by id and all products complete

--- server.js
+++ server.js
@@ -13,6 +13,38 @@ app.get('/', (req, res) => res.send('Build the API!'))
 
 // Build Routes
 
+//All Products
+
+app.get('/api/v1/products', (req, res) => {
+  const products = productsService.findAll()
+  res.json(products)
+})
+
+//Product by ID
+
+app.get('/api/v1/products/:id', (req, res) => {
+  const prodId = Number(req.params.id)
+  const product = productsService.findOneById(prodId)
+  if (!product) {
+    res.json(404).json({error: 'No product found'})
+  }
+  res.json(product)
+})
+
+//Search Product *** Doesnt work **
+
+// app.get('/api/v1/products/search', (req, res) => {
+//   const key = req.query.key
+//   const value = req.query.value
+//   const sortBy = req.query.sort || 'id'
+//   const order = req.query.order || 'ASC'
+//   if (!key || !value) {
+//     res.status(400).json({ error: 'Invalid search query' })
+//     return
+//   }
+//   const products = productsService.search(key, value, sortBy, order)
+//   res.json(products)
+// })
 
 
 app.get('*', (req, res) => res.status(404).send('Page not found'))
